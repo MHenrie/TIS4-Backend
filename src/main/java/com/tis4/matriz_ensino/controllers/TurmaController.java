7a20f39fb9f1edf2de00d3336e524dffa1ad8a1f
MHenrie <silvamarlon87@gmail.com>
2019-09-18 17:13:07 -0300

    api funcionando corretamente

--- src/main/java/com/tis4/matriz_ensino/controllers/TurmaController.java
+++ src/main/java/com/tis4/matriz_ensino/controllers/TurmaController.java
@@ -8,6 +8,7 @@ import com.tis4.matriz_ensino.models.Turma;
 import com.tis4.matriz_ensino.repository.TurmaRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class TurmaController {
--- src/main/java/com/tis4/matriz_ensino/controllers/UsuarioController.java
+++ src/main/java/com/tis4/matriz_ensino/controllers/UsuarioController.java
@@ -12,6 +12,7 @@ import com.tis4.matriz_ensino.models.Usuario;
 import com.tis4.matriz_ensino.repository.UsuarioRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class UsuarioController {
